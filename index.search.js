e6e3066bec7ea547c083b58d9cc8c01b501380f6
lakshithagunasekara <lakshithagunasekara@users.noreply.github.com>
2024-01-22 03:38:33 +0000

    deploy: 30cb0da23cff57ff632e0d9ef5905a3b3f34fa60

---
+++ index.search.js
@@ -0,0 +1,100 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "Important Dates Event Date Special Session Proposal Deadline Dec 1, 2022 Special Session Proposal Acceptance Notification Dec 15, 2022 Regular and Special Session Paper Submission Deadline Feb 28, 2023 Work-in-Progress Paper Submission Deadline Mar 20, 2023 Paper Acceptance Notification Apr 30, 2023 Final Paper Submission Deadline (All) Apr 28, 2023 Tutorial Proposal Deadline Dec 1, 2022 Tutorial Acceptance Notification Dec 15, 2022 Author Registration Deadline Feb 28, 2023 ",
+    "description": "",
+    "tags": null,
+    "title": "For Authors",
+    "uri": "/for_authors/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Find out how to create and organize your content quickly and intuitively.\n",
+    "description": "",
+    "tags": null,
+    "title": "Content",
+    "uri": "/program/index.html"
+  },
+  {
+    "breadcrumb": "Content",
+    "content": "Papers are solicited for regular technical tracks and special session tracks as Regular papers and Work in progress (WIP) papers.\nWIP papers provide the opportunity to share early-stage research work that have not yet produced the full results to the community. WIP submissions that showcase only ideas without supporting results will not be accepted. Accepted and registered WiP papers will be published in the conference proceedings and in the IEEE Xplore Digital Library similarly to Regular papers.\nPlease note: The Regular papers are limited to six (6) pages of A4 or letter format. Additional two (2) pages can be included for an extra fee. WIP papers follow the regular paper submission guidelines, with a shorter limit of four (4) pages.\nClick here for paper submission templates and other details.\n",
+    "description": "",
+    "tags": null,
+    "title": "Conference Program",
+    "uri": "/program/conferernce_program/index.html"
+  },
+  {
+    "breadcrumb": "Content",
+    "content": "ISIE 2023 will use the new IES paper submission system. Please read the submission instructions below. Paper format and templates are also found further down the page.\nPaper submission site instructions:\nYou will need to register in order to use the system. After registering, wait for the confirmation email and verify your email address. If you don’t get the confirmation email or have some problem with your submission, please contact conferences@ieee-ies.org. When submitting papers in the submission system, please select the correct track: TT01 … TT13 – technical tracks (regular papers) WIP TT01 … WIP TT13 – technical tracks (work-in-progress papers) SS01 … SS14 – special sessions (regular papers) WIP SS01 … WIP SS14 – special sessions (work-in-progress papers) ",
+    "description": "",
+    "tags": null,
+    "title": "Tutorials and Workshops",
+    "uri": "/program/tutorials_and_workshops/index.html"
+  },
+  {
+    "breadcrumb": "For Authors",
+    "content": "ISIE 2023 will use the new IES paper submission system. Please read the submission instructions below. Paper format and templates are also found further down the page.\nPaper submission site instructions:\nYou will need to register in order to use the system. After registering, wait for the confirmation email and verify your email address. If you don’t get the confirmation email or have some problem with your submission, please contact conferences@ieee-ies.org. When submitting papers in the submission system, please select the correct track: TT01 … TT13 – technical tracks (regular papers) WIP TT01 … WIP TT13 – technical tracks (work-in-progress papers) SS01 … SS14 – special sessions (regular papers) WIP SS01 … WIP SS14 – special sessions (work-in-progress papers) ",
+    "description": "",
+    "tags": [
+      "config"
+    ],
+    "title": "Paper Submissions",
+    "uri": "/for_authors/paper_submission/index.html"
+  },
+  {
+    "breadcrumb": "For Authors",
+    "content": "Papers are solicited for regular technical tracks and special session tracks as Regular papers and Work in progress (WIP) papers.\nWIP papers provide the opportunity to share early-stage research work that have not yet produced the full results to the community. WIP submissions that showcase only ideas without supporting results will not be accepted. Accepted and registered WiP papers will be published in the conference proceedings and in the IEEE Xplore Digital Library similarly to Regular papers.\nPlease note: The Regular papers are limited to six (6) pages of A4 or letter format. Additional two (2) pages can be included for an extra fee. WIP papers follow the regular paper submission guidelines, with a shorter limit of four (4) pages.\nClick here for paper submission templates and other details.\n",
+    "description": "",
+    "tags": null,
+    "title": "Call For Papers",
+    "uri": "/for_authors/call_for_papers/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: config",
+    "uri": "/tags/config/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Category :: custom",
+    "uri": "/categories/custom/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": " IEEE Conferernce on Industrial Electronics 2024 Welcome to ISIE 2023 The IEEE ISIE 2023 is the 32nd International Symposium on Industrial Electronics (ISIE), focusing on frontier technologies for industries, applications of electronics, controls, communications, instrumentation and computational intelligence. The objectives of the conference are to provide high quality research and professional interactions for the advancement of science, technology, and fellowship.\nPapers on completed work with new results, or work-in-progress in novel research are encouraged for submission.\nLeaders of European research projects are invited to organise workshops at ISIE 2023.\nTip Financial support of up to US$1500 will be available for attending ISIE2023 through the Students and Young Professionals Paper Assistance Program (SYPA). The application round will close on May 14, 2023.\nImportant Dates Event Date Special Session Proposal Deadline Dec 1, 2022 Special Session Proposal Acceptance Notification Dec 15, 2022 Regular and Special Session Paper Submission Deadline Feb 28, 2023 Work-in-Progress Paper Submission Deadline Mar 20, 2023 Paper Acceptance Notification Apr 30, 2023 Final Paper Submission Deadline (All) Apr 28, 2023 Tutorial Proposal Deadline Dec 1, 2022 Tutorial Acceptance Notification Dec 15, 2022 Author Registration Deadline Feb 28, 2023 ",
+    "description": "",
+    "tags": null,
+    "title": "ISIE 2024",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Categories",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Category :: theming",
+    "uri": "/categories/theming/index.html"
+  }
+]
